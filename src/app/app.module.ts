6e02952589e48e7fa3abebde503856cde226f621
Dalton Tejada <d.tejada@motorcredito.com.do>
2019-11-21 20:35:08 -0400

    menu css establecido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AngularFontAwesomeModule
+    BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
